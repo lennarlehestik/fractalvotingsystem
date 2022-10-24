8388c216ea030ef81348f8fd6c82431128f70154
n0umen0n <46645013+n0umen0n@users.noreply.github.com>
2022-10-24 16:56:55 +0300

    config

--- firebase.js
+++ firebase.js
@@ -9,13 +9,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCsYiRx4tl2AkvX7B-C3HopfTNhaPGafVM",
-  authDomain: "edenfracfront.firebaseapp.com",
-  projectId: "edenfracfront",
-  storageBucket: "edenfracfront.appspot.com",
-  messagingSenderId: "820757376798",
-  appId: "1:820757376798:web:fe2597de389de461db630f",
-  measurementId: "G-7RGSZLT8GC",
+  apiKey: "AIzaSyBdGoZcC3UL2vU110PR8aZA1XzV9B4Jv0Q",
+  authDomain: "spanishfractal.firebaseapp.com",
+  projectId: "spanishfractal",
+  storageBucket: "spanishfractal.appspot.com",
+  messagingSenderId: "581958750248",
+  appId: "1:581958750248:web:a70f5633e51af181ce651d"
 };
 
 // Initialize Firebase
